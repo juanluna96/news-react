8eb4160a9eb40d3a38a15c1d067dcb6c6dac16dd
juanluna96 <juandluna17@gmail.com>
2021-02-22 13:30:50 -0500

    Creando un Hook para el Select

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './Formulario.module.css';
+import useSelect from '../hooks/useSelect';
 
 const Formulario = () => {
+
+    // Utilizar custom hooks
+    const [categoria, SelectNoticias] = useSelect();
+
     return (
         <div className={ `${styles.buscador} row` }>
             <div className="col s12 m8 offset-m2">
                 <form action="">
                     <h2 className={ styles.heading }>Encuentra las noticias por categorías</h2>
                     <div className="input-field col s12">
+                        <SelectNoticias></SelectNoticias>
                         <input type="submit" className={ `${styles.btn_block} btn-large amber darken-2` } value="Buscar" />
                     </div>
                 </form>
